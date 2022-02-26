2fb62418a7304157ac183f296dc32437a9434b0f
satyampgt4 <satyamgpt44@gmail.com>
2022-02-26 10:23:41 +0530

    rename database file and add path in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-const app = require("./router");
-const db = require("./database");
+const app = require("./Router/router");
+const db = require("./Database/databse");
 const port = process.env.PORT || 5000;
 global.db = db;
 
