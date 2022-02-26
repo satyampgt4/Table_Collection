c80197b47af83f974d1eef9a645d85ee9f05b0f3
satyampgt4 <satyamgpt44@gmail.com>
2022-02-26 10:19:11 +0530

    create database.js and config it to conncect with database

---
+++ Database/databse.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host:process.env.DB_HOST,
+
+  user: process.env.DB_USER,
+
+  password: process.env.DB_PASSWORD,
+
+  database: process.env.DB_DATABASE,
+});
+
+// create connection to database
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+
+db.connect((err) => {
+  if (err) throw err;
+
+  console.log("MySql Connected");
+});
+module.exports = db;
