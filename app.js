755bf8e56d3b1ddf283383e2e9e2bf131c882265
satyampgt4 <satyamgpt44@gmail.com>
2022-02-27 14:43:09 +0530

    fix typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = require("./Router/router");
-const db = require("./Database/databse");
+const db = require("./Database/database");
 const port = process.env.PORT || 5000;
 global.db = db;
 
