124bea4b5fea1633235ec90895fc940b8164c281
satyampgt4 <satyamgpt44@gmail.com>
2022-03-02 22:40:29 +0530

    add checkAuthenticated function ,sign out and other get post methord

--- Router/router.js
+++ Router/router.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const session = require('express-session');
+const cookieParser = require('cookie-parser')
+// google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = process.env.CLIENT_ID
+const client = new OAuth2Client(CLIENT_ID);
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -9,20 +15,65 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(cookieParser());
 
-const { dashboard } = require("./Routes/user");
-const  {login} = require("./Routes/auth")
-const { frametable,createtable } = require("./Routes/table");
+const { dashboard,showalltable,showtable } = require("./Routes/user");
+const  {login,logout} = require("./Routes/auth")
+const { frametable,createtable,generatetable } = require("./Routes/newtable");
+const  {addnewrow,verifynewrow,editrow,verifyedit} = require("./Routes/table");
 // routes for the app
 //GET
-app.get("/dashboard ", dashboard);
 app.get("/login", login);
-app.get("/", dashboard);
-app.get("/dashboard", dashboard);
+app.get("/", login);
+app.get("/dashboard",checkAuthenticated, dashboard);
+app.get("/createtable",checkAuthenticated, createtable);
+app.get("/mytable",checkAuthenticated, showalltable);
+app.get('/logout',signOut,logout)
 
-app.get("/createtable", createtable);
 
 
-app.post("/createtable",frametable)
 
+app.post('/addnewrow',checkAuthenticated,addnewrow);
+app.post('/verifynewrow',checkAuthenticated,verifynewrow);
+app.post("/createtable",checkAuthenticated,frametable);
+app.post("/mytable",checkAuthenticated,showtable);
+app.post("/frametable",checkAuthenticated,generatetable);
+app.post("/edit",checkAuthenticated,editrow);
+app.post("/verifyedit",checkAuthenticated,verifyedit);
+
+    
+  
+  
+  function checkAuthenticated(req, res, next){
+  
+    let token = req.cookies['session-token'];
+  
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+        // console.log(err);
+          res.redirect('/login')
+      })
+  
+  }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    });
+  }
+  
 module.exports = app;
