fdb18b9687c91370473500872ff86188d2974384
satyampgt4 <satyamgpt44@gmail.com>
2022-03-02 22:53:26 +0530

    implement showalltable, showtable

--- Router/Routes/user.js
+++ Router/Routes/user.js
@@ -1,11 +1,61 @@
 module.exports = {
     
     dashboard: (req, res) => {
+        // console.log(req.user);
         res.render("dashboard.ejs", {
             title: "Welcome to Panna Bank View Account",
-            user : "Guest",
+            user : req.user.name,
             message : ""
           });
         
+    },
+    showalltable: (req, res) => {
+        
+        let email = req.user.email;
+        let query = `SELECT tablename FROM manager WHERE email="${email}";`;
+        db.query(query, (err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            res.render("mytable.ejs", {
+              title: "Welcome to Panna Bank View Account",
+              tables: result,
+              user: req.user.name,
+              message: "",
+  
+            });
+        });
+       
+    },
+    showtable: (req, res) => {
+        
+        let tablename = req.body.tablename;
+        // console.log(tablename);
+        let query = `DESCRIBE ${tablename};`;
+        let header = {};
+        db.query(query, (err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            console.log(result);
+            header = result;
+        });
+         query = `SELECT *  FROM  ${tablename};`;
+        db.query(query, (err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            console.log(header);
+            // console.log(result);
+            res.render("showtable.ejs", {
+                title: "Welcome to Table collection",
+                user : req.user.name,
+                message : "",
+                table : result,
+                tableheader : header,
+                tname: tablename,
+                
+              });
+        });
     },
 };
