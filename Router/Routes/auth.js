6793f7d2a43a4826f0deb4cbbdb11593baea5980
satyampgt4 <satyamgpt44@gmail.com>
2022-03-02 22:55:01 +0530

    implement logout

--- Router/Routes/auth.js
+++ Router/Routes/auth.js
@@ -1,11 +1,17 @@
 module.exports = {
-    
-    login: (req, res) => {
-        res.render("login.ejs", {
-            title: "Welcome to Table Collection ",
-            user : "Guest",
-            message : ""
-          });
-        
-    },
+
+  login: (req, res) => {
+    res.render("login.ejs", {
+      title: "Welcome to Table Collection ",
+      user: "Guest",
+      message: ""
+    });
+
+  },
+  logout: (req, res) => {
+
+    res.clearCookie('session-token');
+    res.redirect('/login');
+  },
+  
 };
