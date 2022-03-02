86b7532d4fae92030fc78c421e502ca1e2aebbce
satyampgt4 <satyamgpt44@gmail.com>
2022-03-03 02:06:40 +0530

    fix logout add filter

--- Router/router.js
+++ Router/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const session = require('express-session');
 const cookieParser = require('cookie-parser')
+// const getAuth = require("firebase/auth").getAuth;
+// const signOut = require("firebase/auth").signOut
+// firebase.initializeApp(config);
+
 // google Auth
 const {OAuth2Client} = require('google-auth-library');
 const CLIENT_ID = process.env.CLIENT_ID
@@ -17,18 +21,27 @@ app.use(session({
 }));
 app.use(cookieParser());
 
-const { dashboard,showalltable,showtable } = require("./Routes/user");
+const { dashboard,showalltable,showtable,filter } = require("./Routes/user");
 const  {login,logout} = require("./Routes/auth")
 const { frametable,createtable,generatetable } = require("./Routes/newtable");
 const  {addnewrow,verifynewrow,editrow,verifyedit} = require("./Routes/table");
 // routes for the app
+
+
+// const auth = getAuth();
+// signOut(auth).then(() => {
+//   // Sign-out successful.
+// }).catch((error) => {
+//   // An error happened.
+// });
+
 //GET
 app.get("/login", login);
 app.get("/", login);
 app.get("/dashboard",checkAuthenticated, dashboard);
 app.get("/createtable",checkAuthenticated, createtable);
 app.get("/mytable",checkAuthenticated, showalltable);
-app.get('/logout',signOut,logout)
+app.get('/logout',logout)
 
 
 
@@ -37,6 +50,7 @@ app.post('/addnewrow',checkAuthenticated,addnewrow);
 app.post('/verifynewrow',checkAuthenticated,verifynewrow);
 app.post("/createtable",checkAuthenticated,frametable);
 app.post("/mytable",checkAuthenticated,showtable);
+app.post("/filter",checkAuthenticated,filter);
 app.post("/frametable",checkAuthenticated,generatetable);
 app.post("/edit",checkAuthenticated,editrow);
 app.post("/verifyedit",checkAuthenticated,verifyedit);
@@ -70,10 +84,6 @@ app.post("/verifyedit",checkAuthenticated,verifyedit);
       })
   
   }
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-    });
-  }
-  
+
+
 module.exports = app;
