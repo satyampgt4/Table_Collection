05fccc8597ab252c13b2042f4691e6ffb60c4694
satyampgt4 <satyamgpt44@gmail.com>
2022-03-03 09:51:41 +0530

    update middaleware rename view to View

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ global.db = db;
 
 // configure middleware
 app.set("port", process.env.port || port); // set express to use this port
-app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
+app.set("Views", __dirname + "/Views"); // set express to look in this folder to render our view
 app.set("view engine", "ejs"); // configure template engine
 
 app.use(express.urlencoded({ extended: false }));
